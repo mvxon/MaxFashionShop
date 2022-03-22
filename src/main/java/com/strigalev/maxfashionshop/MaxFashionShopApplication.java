cd8ed54f055e873949a33069909603e8b38159d5
mvxon <morentetawer@gmail.com>
2022-03-22 15:22:05 +0300

    Initialize Spring Boot Project, add necessary dependencies

---
+++ src/main/java/com/strigalev/maxfashionshop/MaxFashionShopApplication.java
@@ -0,0 +1,13 @@
+package com.strigalev.maxfashionshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxFashionShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaxFashionShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/strigalev/maxfashionshop/MaxFashionShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strigalev.maxfashionshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxFashionShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
